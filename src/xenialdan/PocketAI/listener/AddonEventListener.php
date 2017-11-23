edcf1bee2091d69ddb8f7d8eb2442d1d3ddab4f5
thebigsmileXD <thebigsmilexd@gmail.com>
2017-11-23 11:50:36 +0100

    Fix duplicate execution issues

--- src/xenialdan/PocketAI/listener/AddonEventListener.php
+++ src/xenialdan/PocketAI/listener/AddonEventListener.php
@@ -6,10 +6,8 @@ use pocketmine\event\entity\EntitySpawnEvent;
 use pocketmine\event\entity\ExplosionPrimeEvent;
 use pocketmine\event\Listener;
 use pocketmine\plugin\Plugin;
-use xenialdan\PocketAI\API;
-use xenialdan\PocketAI\EntityProperties;
-use xenialdan\PocketAI\events\AddonEvent;
 use xenialdan\PocketAI\entitytype\AIEntity;
+use xenialdan\PocketAI\events\AddonEvent;
 
 /**
  * Class AddonEventListener
@@ -50,12 +48,11 @@ class AddonEventListener implements Listener{
 
 	public function onAddonEvent(AddonEvent $event){
 		switch ($event->getEvent()){
-			case "minecraft:entity_spawned":{//TODO move all handling / getting / calling into EntityProperties
+			case "minecraft:entity_spawned": {//TODO move all handling / getting / calling into EntityProperties
 				$entityProperties = $event->getEntity()->getEntityProperties();
 				$behaviours = $entityProperties->getBehaviours();
-				foreach ($behaviours["minecraft:entity"]["events"] as $behaviourEvent => $behaviourEvent_data){
-					if(!$event->getEvent() === $behaviourEvent) continue;
-					$entityProperties->applyEvent($behaviourEvent_data);
+				if (!is_null($behaviours["minecraft:entity"]["events"][$event->getEvent()] ?? null)){
+					$entityProperties->applyEvent($behaviours["minecraft:entity"]["events"][$event->getEvent()]);
 				}
 			}
 		}
