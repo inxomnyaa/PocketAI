b3e10f9646c76f0315b53156c6fbf8c516ab2f0d
thebigsmileXD <thebigsmilexd@gmail.com>
2018-09-14 12:41:25 +0200

    Fix find* functions, add _identifier, add getName to BaseComponent

---
+++ src/xenialdan/PocketAI/component/minecraft/_identifier.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace xenialdan\PocketAI\component\minecraft;
+
+use xenialdan\PocketAI\component\BaseComponent;
+use xenialdan\PocketAI\entitytype\AIEntity;
+use xenialdan\PocketAI\entitytype\AIProjectile;
+
+class _type_family extends BaseComponent
+{
+    protected $name = "minecraft:type_family";
+    /** @var array $family List of family names */
+    public $family;
+
+
+    /**
+     * Defines the families this entity belongs to.
+     * _type_family constructor.
+     * @param array $values
+     */
+    public function __construct(array $values = [])
+    {
+        $this->family = $values['family'] ?? $this->family;
+
+    }
+
+    /**
+     * Applies the changes to the mob
+     * @param AIEntity|AIProjectile $entity
+     */
+    public function apply($entity): void
+    {
+        // TODO: Implement apply() method.
+    }
+
+    /**
+     * Removes the changes from the mob
+     * @param AIEntity|AIProjectile $entity
+     */
+    public function remove($entity): void
+    {
+        // TODO: Implement remove() method.
+    }
+}
